996f8d3c80e7b6ecc14ef37285715b1021623071
Christopher Chan <36608720+chrischank@users.noreply.github.com>
2020-02-29 22:47:41 +0100

    Initial commit

---
+++ MB2_NTL_BUG.R
@@ -0,0 +1,15 @@
+######################################################################
+#Cluster timeseries analysis of Nightlights in Bulgaria              #
+#MB2 Introduction to Programming and Geostatistics (MSc. EAGLE, JMUW)#
+#Yan Chak Christopher, Chan (s382722) christopherchank@gmail.com     #
+#Date                                                                #
+######################################################################
+
+setwd("C:/Users/s1526/Dropbox/MB2_IPG(s382722)/MB2_NTL_BUG")
+
+#Libraries----
+library(RStoolbox)
+library(sp)
+library(viridis)
+library(rasterVis)
+library(tidyverse)

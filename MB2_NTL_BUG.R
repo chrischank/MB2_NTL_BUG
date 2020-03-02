a82544863027cc1d5df0ec46b69a6620b6ac0e86
Christopher Chan <36608720+chrischank@users.noreply.github.com>
2020-03-02 17:50:30 +0100

    2nd commit

--- MB2_NTL_BUG.R
+++ MB2_NTL_BUG.R
@@ -13,3 +13,17 @@ library(sp)
 library(viridis)
 library(rasterVis)
 library(tidyverse)
+
+#Ingest data----
+
+#Create paths
+path_2014 <- "D:/MB2_DATA/201401_201412"
+path_2015 <- "D:/MB2_DATA/201501_201512"
+path_2016 <- "D:/MB2_DATA/201601_201612"
+path_2017 <- "D:/MB2_DATA/201701_201712"
+path_2018 <- "D:/MB2_DATA/201801_201812(NA06)"
+path_2019 <- "D:/MB2_DATA/201901_201904"
+
+#STACK & GREP vcmslcfg avg.rade9h
+avg_rade_2014 <- grep("*avg.rade9h", list.files(path=path_2014, pattern="*avg.rade9h.tif$"), value=TRUE)
+
